321ff11f245856cbe9c6c862986da5067c938c96
davebalino <dominicdavebalino@gmail.com>
2023-08-11 16:55:52 +0800

    4-55 08-11 commit

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-    import { createUserWithEmailAndPassword, sendEmailVerification, getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { createUserWithEmailAndPassword, sendEmailVerification, getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: process.env.API_KEY,
-        authDomain: process.env.AUTH_DOMAIN,
-        projectId: process.env.PROJECT_ID,
-        storageBucket: process.env.STORAGE_BUCKET,
-        messagingSenderId: process.env.MESSAGING_SENDER_ID,
-        appId: process.env.APP_ID
-    };
+// Your web app's Firebase configuration html-10 save 3
+const firebaseConfig = {
+    apiKey: "AIzaSyAPb9NizAmyshhglpk1NombXSFgdfaQ2vg",
+    authDomain: "eprogress-account-database.firebaseapp.com",
+    projectId: "eprogress-account-database",
+    storageBucket: "eprogress-account-database.appspot.com",
+    messagingSenderId: "72882320286",
+    appId: "1:72882320286:web:822235eb2be05e86b61c8f"
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-    // Function to handle form submission
-    document.getElementById("submit").addEventListener('click', function(e) {
-        e.preventDefault();
+// Function to handle form submission
+document.getElementById("submit").addEventListener('click', function(e) {
+    e.preventDefault();
 
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-        // Perform user registration here
-        createUserWithEmailAndPassword(getAuth(app), email, password)
-            .then((userCredential) => {
-                // User account created successfully
-                const user = userCredential.user;
+    // Perform user registration here
+    createUserWithEmailAndPassword(getAuth(app), email, password)
+        .then((userCredential) => {
+            // User account created successfully
+            const user = userCredential.user;
 
-                // Send email verification
-                sendEmailVerification(user)
-                    .then(() => {
-                        alert("Sign Up successful. Please check your email for verification.");
+            // Send email verification
+            sendEmailVerification(user)
+                .then(() => {
+                    alert("Sign Up successful. Please check your email for verification.");
 
-                        // Redirect to sign-in page on successful sign-up
-                        window.location.href = "index.html";
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                        alert("Sign Up successful, but failed to send verification email.");
-                    });
-            })
-            .catch((error) => {
-                console.error(error);
-                alert("Error signing up: " + error.message);
-            });
-    });
+                    // Redirect to sign-in page on successful sign-up
+                    window.location.href = "index.html";
+                })
+                .catch((error) => {
+                    console.error(error);
+                    alert("Sign Up successful, but failed to send verification email.");
+                });
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("Error signing up: " + error.message);
+        });
+});
