1d254a0bbdf8db985d672febc2c18c471122a523
davebalino <dominicdavebalino@gmail.com>
2023-08-14 15:11:52 +0800

    main

--- js/show-pass.js
+++ js/show-pass.js
@@ -15,16 +15,3 @@ showPasswordCheckbox.addEventListener("change", function () {
         confirmPasswordInput.type = "password";
     }
 });
-
-
-// Add event listener to checkbox
-// const showPasswordCheckbox = document.getElementById("showPasswordCheckbox");
-// const passwordInput = document.getElementById("password");
-
-// showPasswordCheckbox.addEventListener("change", function() {
-//     if (this.checked) {
-//         passwordInput.type = "text";
-//     } else {
-//         passwordInput.type = "password";
-//     }
-// });
